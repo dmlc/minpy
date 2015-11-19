4a41348d4e0af8fd21bbc7479540ec6aaaccc5e6
haoran <hrwangchengdu@hotmail.com>
2015-11-19 16:18:54 +0800

    Add autograd style to logisitic_regression; Add Convenet example

--- examples/logistic_regression_adv_autograd_style.py
+++ examples/logistic_regression_adv_autograd_style.py
@@ -5,14 +5,14 @@ import minpy.caffe as caffe
 def sigmoid(x):
     return 0.5 * (np.tanh(x) + 1)
 
-def predict(layer, inputs):
+def predict(weight, layer, inputs):
     # original code:
-    # return sigmoid(np.dot(inputs, weights)
-    return sigmoid(layer.ff(inputs))
+    # return sigmoid(np.dot(inputs, weight)
+    return sigmoid(layer.ff(inputs, weight))
 
-def loss_func(layer, inputs, targets):
+def loss_func(weight, layer, inputs, targets):
     def loss_theta(layer):
-        pred = predict(layer, inputs)
+        pred = predict(weight, layer, inputs)
         return -np.sum(np.log(pred * targets)) # negative log likelihood
     return loss_theta
 
@@ -21,15 +21,16 @@ num_inputs = 128
 num_outputs = 256
 inputs = np.random.randn((num_samples, num_inputs))
 targets = np.random.randn((num_samples, num_outputs))
-# original code:
-# weights = np.random.randn((num_inputs, num_outputs))
+weights = np.random.randn((num_inputs, num_outputs))
+
+# additional caffe layer statement
 inner_product_layer = caffe.InnerProductLayer(
         input_shapes = (num_samples, num_inputs),
         num_outputs  = num_outputs)
 
-grad_func = minpy.grad(loss_func(inner_product_layer, inputs, targets))
+# original code:
+# grad_func = minpy.grad(loss_func(weights, inputs, targets))
+grad_func = minpy.grad(loss_func(weights, inner_product_layer, inputs, targets))
 
 for i in range(100):
-    # original code:
-    # weights -= grad_func(weights) * 0.01
-    inner_product_layer.get_learnable_params()[0] -= grad_func(inner_product_layer) * 0.01
+    weights -= grad_func(weights) * 0.01
