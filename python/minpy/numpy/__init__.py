6b0ebe079a84242eabd4edcd884951b9246c368f
Yutian Li <hotpxless@gmail.com>
2016-01-30 10:01:23 -0800

    [master] Dynamic lookup for module minpy.numpy.

--- python/minpy/numpy/__init__.py
+++ python/minpy/numpy/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import sys
 
-from minpy.array_variants.numpy import *
-from minpy.array_variants.mxnet import *
+
+class Module(object):
+    """Module level dynamic lookup."""
+
+    def __getattr__(self, name):
+        pass
+
+sys.modules[__name__] = Module()
