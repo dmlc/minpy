9c59c55993d74c8066fd657bda0eac8532a960cf
Yutian Li <hotpxless@gmail.com>
2016-01-30 09:05:46 -0800

    [master] Put node into array.

--- python/minpy/array.py
+++ python/minpy/array.py
@@ -3,22 +3,25 @@
 """Base type for arrays."""
 from .utils import log
 from .utils import common
-import minpy.numpy as mnp
-
-import numpy as np
-import mxnet as mx
+import minpy.numpy
+import mxnet
 
 logger = log.get_logger(__name__)
+
 
 class ArrayType(common.AutoNumber):
     """Enumeration of types of arrays."""
     NUMPY = ()
     MXNET = ()
 
+
 class ArrayTypeMissingError(ValueError):
     pass
+
+
 class UnknownArrayTypeError(ValueError):
     pass
+
 
 class Array(object):
     """Base array type that provides convenient methods
@@ -30,30 +33,38 @@ class Array(object):
     Member:
         _data: A dict type { array_type : array_data }
     """
-    __slots__ = ['_data']
+
+    _node = Node()  # TODO derivative info
+    _data = dict()  # TODO real data
 
     def __init__(self, data):
         self._data = data
+        # TODO assert data as either numpy.array or mxnet.ndarray, or throw
+        # exception
 
     """ Return whether array data of given type exists in the underlying storage """
+
     def has_type(self, t):
         return t in self._data.keys()
 
     """ Get array data of given type. Raise exception if the type is missing """
+
     def get_data(self, t):
-        if not self.has_type(t):
-            raise ArrayTypeMissingError('Array data of type {} not found.'.format(t))
+        if t not in self._data:
+            raise ArrayTypeMissingError(
+                'Array data of type {} not found.'.format(t))
         return self._data[t]
 
     """ Create data of given type """
+
     def create_data(self, t):
-        if not self.has_type(t):
+        if t not in self._data:
             if t == ArrayType.NUMPY:
                 mxarray = self.get_data(ArrayType.MXNET)
                 self._data[ArrayType.NUMPY] = mxarray.asnumpy()
             elif t == ArrayType.MXNET:
                 nparray = self.get_data(ArrayType.NUMPY)
-                self._data[ArrayType.MXNET] = mx.nd.array(nparray)
+                self._data[ArrayType.MXNET] = mxnet.nd.array(nparray)
             else:
                 raise UnknownArrayTypeError('Unknown array type {}.'.format(t))
 
@@ -61,47 +72,48 @@ class Array(object):
     def shape(self):
         return self._data.values()[0].shape
 
+    # TODO special function redirection and __getattr__ redirection
     def __neg__(self):
-        return mnp.negate(self)
+        return minpy.numpy.negate(self)
 
     def __add__(self, other):
-        return mnp.add(self, other)
+        return minpy.numpy.add(self, other)
 
     def __sub__(self, other):
-        return mnp.subtract(self, other)
+        return minpy.numpy.subtract(self, other)
 
     def __mul__(self, other):
-        return mnp.multiply(self, other)
+        return minpy.numpy.multiply(self, other)
 
     def __div__(self, other):
-        return mnp.divide(self, other)
+        return minpy.numpy.divide(self, other)
 
     def __truediv__(self, other):
-        return mnp.true_divide(self, other)
+        return minpy.numpy.true_divide(self, other)
 
     def __pow__(self, other):
-        return mnp.power(self, other)
+        return minpy.numpy.power(self, other)
 
     def __mod__(self, other):
-        return mnp.mod(self, other)
+        return minpy.numpy.mod(self, other)
 
     def __radd__(self, other):
-        return mnp.add(other, self)
+        return minpy.numpy.add(other, self)
 
     def __rsub__(self, other):
-        return mnp.subtract(other, self)
+        return minpy.numpy.subtract(other, self)
 
     def __rmul__(self, other):
-        return mnp.multiply(other, self)
+        return minpy.numpy.multiply(other, self)
 
     def __rdiv__(self, other):
-        return mnp.divide(other, self)
+        return minpy.numpy.divide(other, self)
 
     def __rtruediv__(self, other):
-        return mnp.true_divide(other, self)
+        return minpy.numpy.true_divide(other, self)
 
     def __rpow__(self, other):
-        return mnp.power(other, self)
+        return minpy.numpy.power(other, self)
 
     def __rmod__(self, other):
-        return mnp.mod(other, self)
+        return minpy.numpy.mod(other, self)
