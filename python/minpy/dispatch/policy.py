f376a9c0cfa672a4fdd2d57c33c602620b25f6aa
Yutian Li <hotpxless@gmail.com>
2016-01-31 08:35:14 -0800

    [master] Tweak dynamic dispatcher so it could find submodule.

--- python/minpy/dispatch/policy.py
+++ python/minpy/dispatch/policy.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Policy for selecting appropriate function to call."""
-import itertools
-import functools
-import operator
 from .utils import log
-from . import array
-from . import registry
 from . import array_variants as variants
 
 logger = log.get_logger(__name__)
 
+
 class AmbiguousPolicyError(ValueError):
     pass
 
+
 class Policy(object):
     """Policy interface """
+
     def decide(self, candidates, *args, **kwargs):
         raise AmbiguousPolicyError('Unimplemented')
 
 
 class PreferMXNetPolicy(Policy):
     """Perfer using MXNet functions."""
+
     def decide(self, candidates, *args, **kwargs):
         if variants.FunctionType.MXNET in candidates.keys():
             return variants.FunctionType.MXNET
@@ -29,6 +28,7 @@ class PreferMXNetPolicy(Policy):
             return variants.FunctionType.NUMPY
 
 default_policy = Policy()
+
 
 def resolve_name(name, args, kwargs, reg, policy=default_policy):
     """Resolve a function name.
--- python/minpy/numpy/__init__.py
+++ python/minpy/numpy/__init__.py
@@ -2,12 +2,42 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import sys
+import typing
+from ..utils import log
+from ..dispatch import registry
+
+_logger = log.get_logger(__name__)
+
+_old_definitions = {
+    '__name__': __name__,
+    '__loader__': __loader__,
+    '__package__': __package__,
+    '__spec__': __spec__,
+    '__file__': __file__,
+}
+
+try:
+    _old_definitions['__cached__'] = __cached__
+except NameError:
+    pass
+
+
+class DynamicLookupError(KeyError):
+    pass
 
 
 class Module(object):
     """Module level dynamic lookup."""
 
-    def __getattr__(self, name):
-        pass
+    _registry = registry.Registry()
+
+    def __getattr__(self, name: str) -> typing.Any:
+        _logger.info('Look up name {}'.format(name))
+        # Special members for internal use.
+        if name == '__registry__':
+            return self._registry
+        elif name in _old_definitions:
+            return _old_definitions[name]
+        raise DynamicLookupError()
 
 sys.modules[__name__] = Module()
