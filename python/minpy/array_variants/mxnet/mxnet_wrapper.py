0375edd09a67ed4917d0b4749ec9a09df86596d1
Yutian Li <hotpxless@gmail.com>
2016-04-25 22:45:02 +0800

    [master] Subtyping of function types in wrapper.

--- python/minpy/array_variants/mxnet/mxnet_wrapper.py
+++ python/minpy/array_variants/mxnet/mxnet_wrapper.py
@@ -10,10 +10,8 @@ def wrap_namespace(ns, reg, prim_wrapper):
     :param reg: Registry into which functions are to be wrapped.
     :param prim_wrapper: Wrapper to convert a raw function to primitive
     """
-    unchanged_types = {float, int, type(None), type}
     function_types = {types.FunctionType, types.BuiltinFunctionType}
-
     for name, obj in ns.items():
-        if type(obj) in function_types:
+        if len(list(filter(lambda x: isinstance(obj, x), function_types))) != 0:
             prim = prim_wrapper(obj)
             reg.register(name, prim)
