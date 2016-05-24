9bf02902fbca77a5a579891b3bb982b8ec3d0c77
haoran <hrwangchengdu@hotmail.com>
2016-05-24 09:46:44 +0800

    [cs231n] Add model params access interface.

--- examples/cs231n/assignment2/cs231n/classifiers/model.py
+++ examples/cs231n/assignment2/cs231n/classifiers/model.py
@@ -4,6 +4,14 @@ import minpy
 from minpy.array_variants import ArrayType
 from minpy.core import wraps, minpy_to_numpy
 
+class ParamsNameNotFoundError(ValueError):
+    """ Error of not existed name during accessing model params """
+    pass
+
+class UnknownAccessModeError(ValueError):
+    """ Error of unexpected mode during accessing model params """
+    pass
+
 
 class ModelBase(object):
   __metaclass__ = abc.ABCMeta
@@ -11,6 +19,19 @@ class ModelBase(object):
   def __init__(self, conv_mode = 'lazy'):
     self.convert_mode = conv_mode
     self.data_target_cnt = 2
+    self.params = {}
+
+  def get_param(self, name, mode = 'numpy'):
+    if name in self.params:
+      if mode == 'numpy':
+        return minpy_to_numpy(self.params[name])
+      elif mode == 'minpy':
+        return self.params[name]
+      else:
+        raise UnknownAccessModeError('unexpected mode {} in accessing model\'s params', mode)
+    else:
+      raise ParamsNameNotFoundError('not found param {} in model'.format(name))
+      
 
   def loss(self, X, y = None):
     res = wraps(self.convert_mode)(self.loss_and_derivative)(X, y)
