bee14ac54747d68298c2961e2a14c6befd87f2a8
Yutian Li <hotpxless@gmail.com>
2016-06-13 19:35:59 +0800

    [master] Register with PyPI.

--- python/setup.py
+++ python/setup.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 import setuptools
 
-setuptools.setup(name='minpy',
-                 version='0.0.1',
-                 packages=setuptools.find_packages(),
-                 install_requires=[
-                     'numpy',
-                     'mxnet',
-                 ],
-                 url='https://github.com/dmlc/minpy')
+setuptools.setup(
+    name='minpy',
+    version='0.0.1',
+    description='Pure NumPy practice with third-party operator integration.',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'mxnet',
+    ],
+    url='https://github.com/dmlc/minpy')
