1f1db5f8c79d947e3f61c935cf327c3ef56c4aaf
Yutian Li <hotpxless@gmail.com>
2016-06-21 16:32:39 +0800

    [master] Add versioning.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import setuptools
+import os.path
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    version = f.readline().strip()
 
 setuptools.setup(
     name='minpy',
-    version='0.0.3',
+    version=version,
     description='Pure NumPy practice with third-party operator integration.',
     packages=setuptools.find_packages(),
     install_requires=[
         'numpy',
         'mxnet',
+        'semantic_version',
     ],
     url='https://github.com/dmlc/minpy')
