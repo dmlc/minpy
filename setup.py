17d9cef63309a41c1b4622a828dd6f5c2fe5efed
Yutian Li <hotpxless@gmail.com>
2016-06-22 22:45:30 +0800

    [master] Add dependency.

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'numpy',
+        'scipy',
+        'pillow',
         'mxnet',
         'semantic-version',
     ],
