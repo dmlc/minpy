7f4eefa674a02ebf92f83671fd3c8f23e524b4d1
haoran <hrwangchengdu@hotmail.com>
2016-06-27 11:41:56 +0800

    [master] Add mxnet symbol test case.

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,4 +1,6 @@
 import numpy as np
+import mxnet as mx
+from minpy import core
 import minpy.nn.layers as layers
 import minpy.utils.gradient_checker as gradient_checker
 import minpy.dispatch.policy as plc
@@ -42,11 +44,25 @@ def test_softmax():
     x = rng.randn(20, 10)
     gradient_checker.quick_grad_check(check_fn, x, rs=rng)
 
+def test_mxnet_affine():
+    xshape = (10, 40)
+    fake_y = np.zeros([10, 20])
+    weights = rng.randn(20, 40) - 0.5
+    inputs = mx.sym.Variable(name='x')
+    fc = mx.sym.FullyConnected(name='fc', data=inputs, num_hidden=20)
+    f = core.function(fc, [('x', xshape)])
+    def check_fn(x):
+        return layers.l2_loss(f(x = x, fc_weight = weights), fake_y)
+
+    x = rng.randn(*xshape) - 0.5
+    gradient_checker.quick_grad_check(check_fn, x, rs=rng)
+
 def main():
     test_affine()
     test_relu()
     test_batchnorm()
     test_softmax()
+    test_mxnet_affine()
 
 if __name__ == '__main__':
     main()
