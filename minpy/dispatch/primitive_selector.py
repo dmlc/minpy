c7659e9bb6dc9b5e28cd1f2ac9df827ccee77e0d
Yutian Li <hotpxless@gmail.com>
2016-07-10 17:45:19 +0800

    [mlp-tutorial] Refine example.

--- minpy/dispatch/primitive_selector.py
+++ minpy/dispatch/primitive_selector.py
@@ -10,8 +10,9 @@ from minpy.dispatch import policy
 
 _logger = log.get_logger(__name__)
 
+
 class PrimitiveSelector(object):
-    """ Primitive selector class that behaves like normal function but instead pass all the
+    """Primitive selector class that behaves like normal function but instead pass all the
     arguments to the policy to choose appropriate primitive.
     """
     __slots__ = ['_name', '_registry', '_policy']
@@ -23,14 +24,18 @@ class PrimitiveSelector(object):
 
     @property
     def name(self):
-        """ Get the name of this function """
+        """Get the name of this function.
+
+        :return: Name of function.
+        """
         return self._name
 
     def __call__(self, *args, **kwargs):
-        """ Call policy to choose the real primitive and then call the returned function with
+        """Call policy to choose the real primitive and then call the returned function with
         the given arguments.
         """
-        prim = policy.resolve_name(self._name, self._registry, self._policy, args, kwargs)
-        _logger.debug('Found primitive "{}" with type {}.'.format(self._name,
-                                                                  prim.typestr))
+        prim = policy.resolve_name(self._name, self._registry, self._policy,
+                                   args, kwargs)
+        _logger.debug('Found primitive "{}" with type {}.'.format(
+            self._name, prim.typestr))
         return prim(*args, **kwargs)
