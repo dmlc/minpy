be2397941217719883e0b3116b63feaebd1acbb4
Yutian Li <hotpxless@gmail.com>
2016-07-19 01:27:03 +0800

    [no-recursion] Examples.

--- examples/basics/multiplication.py
+++ examples/basics/multiplication.py
@@ -1,10 +1,15 @@
+import logging
 import minpy.numpy as np
 import minpy.numpy.random as random
 from minpy import core
+from minpy.dispatch import policy
 
+logging.getLogger('minpy.array').setLevel(logging.DEBUG)
+logging.getLogger('minpy.core').setLevel(logging.DEBUG)
+np.set_policy(policy.OnlyNumpyPolicy())
 
 def f(x, y):
-    return np.dot(x, y)
+    return np.multiply(x, y)
 
 
 def main():
@@ -12,8 +17,10 @@ def main():
     y = random.rand(3, 3)
     print('x: {}'.format(x.asnumpy()))
     print('y: {}'.format(y.asnumpy()))
-    g = core.grad(f)
-    print('grad: {}'.format(g(x, y).asnumpy()))
+    g = core.grad(f, argnum=[0, 1])
+    gr = g(x, y)
+    print('grad_x: {}'.format(gr[0].asnumpy()))
+    print('grad_y: {}'.format(gr[1].asnumpy()))
 
 
 if __name__ == '__main__':
