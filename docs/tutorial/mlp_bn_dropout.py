84ac85ee9664e598100ea559878b75cbcc067e23
Minjie Wang <wmjlyjemaine@gmail.com>
2016-08-23 00:52:39 -0400

    change tutorial doc

--- docs/tutorial/mlp_bn_dropout.py
+++ docs/tutorial/mlp_bn_dropout.py
@@ -7,11 +7,13 @@ num_classes=10
 class TwoLayerNet(ModelBase):
     def __init__(self):
         super(TwoLayerNet, self).__init__()
+        # Define model parameters.
         self.add_param(name='w1', shape=(flattened_input_size, hidden_size)) \
             .add_param(name='b1', shape=(hidden_size,)) \
             .add_param(name='w2', shape=(hidden_size, num_classes)) \
             .add_param(name='b2', shape=(num_classes,)) \
-            .add_param(name='gamma', shape=(hidden_size,), init_rule='constant', init_config={'value': 1.0}) \
+            .add_param(name='gamma', shape=(hidden_size,),
+                       init_rule='constant', init_config={'value': 1.0}) \
             .add_param(name='beta', shape=(hidden_size,), init_rule='constant') \
             .add_aux_param(name='running_mean', value=None) \
             .add_aux_param(name='running_var', value=None)
@@ -25,7 +27,8 @@ class TwoLayerNet(ModelBase):
         y2 = layers.relu(y1)
         # Batch normalization
         y3, self.aux_params['running_mean'], self.aux_params['running_var'] = layers.batchnorm(
-            y2, self.params['gamma'], self.params['beta'], running_mean=self.aux_params['running_mean'], \
+            y2, self.params['gamma'], self.params['beta'],
+            running_mean=self.aux_params['running_mean'],
             running_var=self.aux_params['running_var'])
         # Second affine layer.
         y4 = layers.affine(y3, self.params['w2'], self.params['b2'])
