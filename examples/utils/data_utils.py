c5012d3c023cf77b3b3fd108c3c3c8c0bae44e27
Larry Tang <lryta0@gmail.com>
2016-08-23 15:04:28 +0800

    softmax tutorial code fixed

--- examples/utils/data_utils.py
+++ examples/utils/data_utils.py
@@ -219,6 +219,20 @@ def adding_problem_generator(N, seq_len=6, p=0.5, maxint=50):
     return X, Y
 
 
+""" Generates several clusters of Gaussian points """
+def gaussian_cluster_generator(num_samples=10000, num_features=500, num_classes=5):
+    mu = np.random.rand(num_classes, num_features)
+    sigma = np.ones((num_classes, num_features)) * 0.1
+    num_cls_samples = num_samples / num_classes
+    x = np.zeros((num_samples, num_features))
+    y = np.zeros((num_samples, num_classes))
+    for i in range(num_classes):
+        cls_samples = np.random.normal(mu[i,:], sigma[i,:], (num_cls_samples, num_features))
+        x[i*num_cls_samples:(i+1)*num_cls_samples] = cls_samples
+        y[i*num_cls_samples:(i+1)*num_cls_samples,i] = 1
+    return x, y
+
+
 def load_models(models_dir):
     """Load saved models from disk. This will attempt to unpickle all files in a
     directory; any files that give errors on unpickling (such as README.txt) will
