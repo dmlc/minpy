50e9af42896237e15dadf26157feb7eeff3054ff
Haoran Wang <hrwangchengdu@hotmail.com>
2016-10-12 21:59:05 -0400

    Fix shape in conv example

--- examples/basics/mxnet_convolution.py
+++ examples/basics/mxnet_convolution.py
@@ -9,30 +9,11 @@ def sigmoid(x):
 
 #xshape = (256, 500)
 xshape = (256, 1, 30, 30)
-#needs to reverse. because of mixnet's setting
-tshape = (256, 250)
 inputs = random.rand(*xshape) - 0.5
-targets = np.zeros(tshape)
-truth = random.randint(0, 250, 256)
-targets[np.arange(256), truth] = 1
-
-fc_wshape = (250, 845)
-fc_weight = random.rand(*fc_wshape) - 0.5
-
-fc_bshape = (250, )
-#fc_bias = np.zeros(*fc_bshape)
-fc_bias = random.rand(*fc_bshape) * 0
-
-conv_wshape = (5, 1, 5, 5)
-conv_weight = random.rand(*conv_wshape) - 0.5
-
-conv_bshape = (5, )
-#conv_bias = np.zeros(*conv_bshape)
-conv_bias = random.rand(*conv_bshape) * 0
 
 data = mx.symbol.Variable(name='x')
 conv1 = mx.symbol.Convolution(
-    name='conv', data=data, kernel=(5, 5), num_filter=5)
+    name='conv', data=data, kernel=(5, 5), pad=(1, 1), num_filter=5)
 tanh1 = mx.symbol.Activation(data=conv1, act_type="tanh")
 pool1 = mx.symbol.Pooling(
     data=tanh1, pool_type="max", kernel=(2, 2), stride=(2, 2))
@@ -43,6 +24,16 @@ act = mx.sym.Activation(data=fc, act_type='sigmoid')
 
 f = core.Function(act, {'x': xshape})
 
+
+targets = np.zeros(f.get_one_output_shape())
+truth = random.randint(0, 250, 256)
+targets[np.arange(256), truth] = 1
+
+param_shapes = f._param_shapes
+fc_weight = random.rand(*param_shapes['fc_weight']) - 0.5
+fc_bias = np.zeros(param_shapes['fc_bias']) * 0
+conv_weight = random.rand(*param_shapes['conv_weight']) - 0.5
+conv_bias = np.zeros(param_shapes['conv_bias']) * 0
 
 def predict(inputs, fc_weight, fc_bias, conv_weight, conv_bias):
     #return f( data=[('x', inputs)], weight=[('fc_weight', weights)], ctx=mx.cpu())
