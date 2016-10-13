df2dc82cd5d4619107f1f21011bef8b309a271c7
Larry Tang <lryta0@gmail.com>
2016-10-13 18:48:19 +0800

    Import some numpy objects to np namespace. Import autoblacklist policy to master namespace.

--- minpy/numpy/__init__.py
+++ minpy/numpy/__init__.py
@@ -3,9 +3,10 @@
 """ Mock numpy package """
 #pylint: disable= invalid-name
 import sys
+import numpy
 
 from minpy.array import Value
-from minpy.numpy.mocking import Module
+from minpy.numpy.mocking import Module, NameInjector
 
 from . import random
 
@@ -15,6 +16,11 @@ _old = {
     'random' : random,
 }
 
-_mod = Module(_old)
+injected_name = {'float', 'float16', 'float32', 'float64', 'int', 'int8',
+                 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32',
+                 'uint64', 'bool',
+                 'newaxis',
+                 'e', 'pi', 'inf'}
+_mod = Module(_old, name_injector=NameInjector(injected_name, numpy))
 Value._ns = _mod
 sys.modules[__name__] = _mod
