4c60d77b4987fcfa0e36223a10312e785ee0be87
Larry Tang <lryta0@gmail.com>
2016-10-16 16:52:35 +0800

    Import float, None, type, int objects from numpy into minpy.numpy

--- minpy/numpy/__init__.py
+++ minpy/numpy/__init__.py
@@ -11,16 +11,14 @@ from minpy.numpy.mocking import Module, NameInjector
 from . import random
 
 _old = {
-    '__path__' : __path__,
-    '__name__' : __name__,
-    'random' : random,
+    '__path__': __path__,
+    '__name__': __name__,
+    'random': random,
 }
 
-injected_name = {'float', 'float16', 'float32', 'float64', 'int', 'int8',
-                 'int16', 'int32', 'int64', 'uint8', 'uint16', 'uint32',
-                 'uint64', 'bool',
-                 'newaxis',
-                 'e', 'pi', 'inf'}
-_mod = Module(_old, name_injector=NameInjector(injected_name, numpy))
+_mod = Module(
+    _old,
+    name_injector=NameInjector(
+        numpy, name='numpy injector', injected_type={float, int, type(None), type}))
 Value._ns = _mod
 sys.modules[__name__] = _mod
