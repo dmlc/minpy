ba99e498cab9220e1615036ec697fb4d30bea503
Larry Tang <lryta0@gmail.com>
2016-11-14 20:55:00 +0800

    Reuse functools.wraps

--- minpy/primitive.py
+++ minpy/primitive.py
@@ -164,11 +164,9 @@ class Primitive(object):
 
             def context_wrapper(func):
                 """A context wrapper only for gradient function.
-
-                Note: no need to use functools.wraps to get old function
-                signature.
                 """
 
+                @functools.wraps(func)
                 def wrapped(result):
                     with get_context(result).as_mxnet_context():
                         return func(result)
@@ -177,11 +175,9 @@ class Primitive(object):
 
             def raw_value_wrapper(func):
                 """Unwrap Value for gradient function.
-
-                Note: no need to use functools.wraps to get old function
-                signature.
                 """
 
+                @functools.wraps(func)
                 def wrapped(result):
                     return func(result.get_data(self.type))
 
