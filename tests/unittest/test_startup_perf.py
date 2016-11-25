0110a15684aae43611352b959d404b6e6304bd6c
Haoran Wang <hrwangchengdu@hotmail.com>
2016-11-24 20:38:54 -0500

    Change print to print() for python3

--- tests/unittest/test_startup_perf.py
+++ tests/unittest/test_startup_perf.py
@@ -8,17 +8,17 @@ def test_startup_perf():
     start = time.time()
     np.array([100, 100]).asnumpy()
     end = time.time()
-    print 'First call:', end - start
+    print('First call:', end - start)
     
     start = time.time()
     np.array([100, 100]).asnumpy()
     end = time.time()
-    print 'Second call:', end - start
+    print('Second call:', end - start)
     
     start = time.time()
     np.array([100, 100]).asnumpy()
     end = time.time()
-    print 'Third call:', end - start
+    print('Third call:', end - start)
 
 if __name__ == "__main__":
     test_startup_perf()
