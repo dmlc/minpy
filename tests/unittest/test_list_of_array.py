afd4ea5c21e69abbec0b99103bf6713a63932e3e
ZihengJiang <jzhtomas@gmail.com>
2017-03-09 22:55:34 +0000

    add test_concatenate in unitest/test_list_of_array.py

--- tests/unittest/test_list_of_array.py
+++ tests/unittest/test_list_of_array.py
@@ -7,5 +7,11 @@ def test_stack():
     res = np.stack(arr)
     assert res.shape == (10, 3, 4)
 
+def test_concatenate():
+    arr = [rnd.randn(3, 4) for _ in range(10)]
+    res = np.concatenate(arr, axis=1)
+    assert res.shape == (3, 40)
+
 if __name__  == '__main__':
     test_stack()
+    test_concatenate()
