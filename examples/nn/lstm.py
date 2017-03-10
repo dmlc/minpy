4a5fe627817aeaa49907e80a1473908c2daf1aab
Larry Tang <lryta0@gmail.com>
2017-03-10 08:21:02 +0800

    Add LSTM example

---
+++ examples/nn/lstm.py
@@ -0,0 +1,92 @@
+import minpy.numpy as np
+from minpy.nn import layers
+from minpy.nn.model import ModelBase
+from minpy.nn.solver import Solver
+from minpy.nn.io import NDArrayIter
+from examples.utils.data_utils import adding_problem_generator as data_gen
+
+
+class LSTMNet(ModelBase):
+    def __init__(
+            self,
+            batch_size=100,
+            input_size=2,  # input dimension
+            hidden_size=100,
+            num_classes=1):
+        super(LSTMNet, self).__init__()
+        self.add_param(name='Wx', shape=(input_size, 4*hidden_size))\
+            .add_param(name='Wh', shape=(hidden_size, 4*hidden_size))\
+            .add_param(name='b', shape=(4*hidden_size,))\
+            .add_param(name='Wa', shape=(hidden_size, num_classes))\
+            .add_param(name='ba', shape=(num_classes,))
+
+    def forward(self, X, mode):
+        seq_len = X.shape[1]
+        batch_size = X.shape[0]
+        hidden_size = self.params['Wh'].shape[0]
+        h = np.zeros((batch_size, hidden_size))
+        c = np.zeros((batch_size, hidden_size))
+        for t in xrange(seq_len):
+            h, c = layers.lstm_step(X[:, t, :], h, c, self.params['Wx'], self.params['Wh'],
+                                    self.params['b'])
+        y = layers.affine(h, self.params['Wa'], self.params['ba'])
+        return y
+
+    def loss(self, predict, y):
+        return layers.l2_loss(predict, y)
+
+
+class RNNNet(ModelBase):
+    def __init__(
+            self,
+            batch_size=100,
+            input_size=2,  # input dimension
+            hidden_size=64,
+            num_classes=1):
+        super(RNNNet, self).__init__()
+        self.add_param(name='Wx', shape=(input_size, hidden_size))\
+            .add_param(name='Wh', shape=(hidden_size, hidden_size))\
+            .add_param(name='b', shape=(hidden_size,))\
+            .add_param(name='Wa', shape=(hidden_size, num_classes))\
+            .add_param(name='ba', shape=(num_classes,))
+
+    def forward(self, X, mode):
+        seq_len = X.shape[1]
+        batch_size = X.shape[0]
+        hidden_size = self.params['Wh'].shape[0]
+        h = np.zeros((batch_size, hidden_size))
+        for t in xrange(seq_len):
+            h = layers.rnn_step(X[:, t, :], h, self.params['Wx'], self.params['Wh'],
+                                self.params['b'])
+        y = layers.affine(h, self.params['Wa'], self.params['ba'])
+        return y
+
+    def loss(self, predict, y):
+        return layers.l2_loss(predict, y)
+
+
+def main():
+    model = LSTMNet()
+    x_train, y_train = data_gen(10000)
+    x_test, y_test = data_gen(1000)
+
+    train_dataiter = NDArrayIter(x_train, y_train, batch_size=100, shuffle=True)
+
+    test_dataiter = NDArrayIter(x_test, y_test, batch_size=100, shuffle=False)
+
+    solver = Solver(
+        model,
+        train_dataiter,
+        test_dataiter,
+        num_epochs=10,
+        init_rule='xavier',
+        update_rule='adam',
+        task_type='regression',
+        verbose=True,
+        print_every=20)
+    solver.init()
+    solver.train()
+
+
+if __name__ == '__main__':
+    main()
